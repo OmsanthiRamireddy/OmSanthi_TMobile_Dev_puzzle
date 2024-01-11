f2bdccd63d0e93d5eaf03df1c146e6862b144af3
omsanthi <omsanthi1996@gmail.com>
2024-01-11 19:46:34 +0530

    task 2 implementation changes

---
+++ libs/books/feature/src/lib/book-search/book-search.component.ts
@@ -0,0 +1,65 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Store } from '@ngrx/store';
+import {
+  addToReadingList,
+  clearSearch,
+  getAllBooks,
+  ReadingListBook,
+  searchBooks
+} from '@tmo/books/data-access';
+import { FormBuilder } from '@angular/forms';
+import { Book } from '@tmo/shared/models';
+import { fromEvent } from 'rxjs';
+import { debounceTime, distinctUntilChanged, filter, map } from 'rxjs/operators';
+@Component({
+  selector: 'tmo-book-search',
+  templateUrl: './book-search.component.html',
+  styleUrls: ['./book-search.component.scss']
+})
+export class BookSearchComponent implements OnInit {
+  books: ReadingListBook[];
+  @ViewChild('movieSearchInput', { static: true }) movieSearchInput!: ElementRef;
+  searchForm = this.fb.group({
+    term: ''
+  });
+
+  constructor(
+    private readonly store: Store,
+    private readonly fb: FormBuilder
+  ) {}
+
+  get searchTerm(): string {
+    return this.searchForm.value.term;
+  }
+
+  ngOnInit(): void {
+    this.store.select(getAllBooks).subscribe(books => {
+      this.books = books;
+    });
+  }
+
+  formatDate(date: void | string) {
+    return date
+      ? new Intl.DateTimeFormat('en-US').format(new Date(date))
+      : undefined;
+  }
+
+  addBookToReadingList(book: Book) {
+    this.store.dispatch(addToReadingList({ book }));
+  }
+
+  searchExample() {
+    this.searchForm.controls.term.setValue('javascript');
+    this.searchBooks() ;
+  }
+
+  searchBooks() {
+
+    if (this.searchForm.value.term) {
+      this.searchForm.value.term.pipe(
+        debounceTime(500),this.store.dispatch(searchBooks({ term: this.searchTerm })));
+    } else {
+      this.store.dispatch(clearSearch());
+    }
+  }
+}
